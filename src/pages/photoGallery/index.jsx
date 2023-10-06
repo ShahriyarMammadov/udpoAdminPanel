0c48f2da605f2299b99d56f157c06498e19c9710
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-10-06 18:42:20 +0400

    gallery

--- src/pages/photoGallery/index.jsx
+++ src/pages/photoGallery/index.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./index.scss";
+import { InfoCircleOutlined } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
 
 const PhotoCatalog = () => {
-  return <div>PhotoCatalog</div>;
+  const [form] = Form.useForm();
+
+  return (
+    <div id="photoGallery">
+      <Form form={form} layout="vertical">
+        <Form.Item
+          label="Qalereyanın adı"
+          tooltip="Bu Hissədə Qalereyanın Adı Qeyd Edilməlidir"
+        >
+          <Input placeholder="Qalereyanın adı" />
+        </Form.Item>
+        <Form.Item
+          label=">əkillər"
+          tooltip={{
+            title: "kjnj122334455667788990",
+            icon: <InfoCircleOutlined />,
+          }}
+        >
+          <input type="file" name="" id="" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="dashed">Əlavə Et</Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default PhotoCatalog;
--- src/pages/sorgu/index.jsx
+++ src/pages/sorgu/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { InfoCircleOutlined } from "@ant-design/icons";
-import { Button, Form, Input } from "antd";
+import { Button, DatePicker, Form, Input } from "antd";
 
 const Sorgu = () => {
   const [form] = Form.useForm();
@@ -19,7 +19,7 @@ const Sorgu = () => {
         <Form.Item
           label="1-ci Cavab"
           tooltip={{
-            title: "Tooltip with customize icon",
+            title: "Sorğunun 1-ci Cavabı",
             icon: <InfoCircleOutlined />,
           }}
         >
@@ -28,7 +28,7 @@ const Sorgu = () => {
         <Form.Item
           label="2-ci Cavab"
           tooltip={{
-            title: "Tooltip with customize icon",
+            title: "Sorğunun 2-ci Cavabı",
             icon: <InfoCircleOutlined />,
           }}
         >
@@ -37,7 +37,7 @@ const Sorgu = () => {
         <Form.Item
           label="3-ci Cavab"
           tooltip={{
-            title: "Tooltip with customize icon",
+            title: "Sorğunun 3-cü Cavabı",
             icon: <InfoCircleOutlined />,
           }}
         >
@@ -46,7 +46,7 @@ const Sorgu = () => {
         <Form.Item
           label="4-ci Cavab"
           tooltip={{
-            title: "Tooltip with customize icon",
+            title: "Sorğunun 4-cü Cavabı",
             icon: <InfoCircleOutlined />,
           }}
         >
@@ -55,11 +55,20 @@ const Sorgu = () => {
         <Form.Item
           label="5-ci Cavab"
           tooltip={{
-            title: "Tooltip with customize icon",
+            title: "Sorğunun 5-ci Cavabı",
             icon: <InfoCircleOutlined />,
           }}
         >
           <Input placeholder="Sorğu Cavabları" />
+        </Form.Item>
+        <Form.Item
+          label="Sonuncu Tarix"
+          tooltip={{
+            title: "Sorğunun Bitəcəyi Tarix",
+            icon: <InfoCircleOutlined />,
+          }}
+        >
+          <DatePicker renderExtraFooter={() => "Sorğunun Bitəcəyi Tarix"} />
         </Form.Item>
         <Form.Item>
           <Button type="dashed">Əlavə Et</Button>
