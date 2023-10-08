61692cfee7c1d0f7313c377022fb02a15e2a3258
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-10-08 17:24:42 +0400

    dynamic sorgu

--- src/pages/sorgu/index.jsx
+++ src/pages/sorgu/index.jsx
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { InfoCircleOutlined } from "@ant-design/icons";
 import { Button, DatePicker, Form, Input } from "antd";
+import axios from "axios";
+import "./index.scss";
 
 const Sorgu = () => {
   const [form] = Form.useForm();
+  const [sorguData, setSorguData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getSorguAllData = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:3000/sorgu/getAllSorgu`
+      );
+
+      setSorguData(data?.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSorguAllData();
+  }, []);
+
+  console.log(sorguData);
 
   return (
     <div id="sorgu">
@@ -78,28 +101,41 @@ const Sorgu = () => {
       <hr />
       <h2>Əvvəlki Sorğular və Nəticələri</h2>
 
-      <Form form={form} layout="vertical" disabled>
-        <Form.Item label="Sorğunun Başlığı">
-          <Input placeholder="Sorğunun Başlığı" />
-        </Form.Item>
-        <hr />
-        <p style={{ fontWeight: "700" }}>Cavablar</p>
-        <Form.Item label="1-ci Cavab - 10%">
-          <Input placeholder="Sorğu Cavabları" />
-        </Form.Item>
-        <Form.Item label="2-ci Cavab - 75%">
-          <Input placeholder="Sorğu Cavabları" />
-        </Form.Item>
-        <Form.Item label="3-ci Cavab - 1%">
-          <Input placeholder="Sorğu Cavabları" />
-        </Form.Item>
-        <Form.Item label="4-ci Cavab - 2%">
-          <Input placeholder="Sorğu Cavabları" />
-        </Form.Item>
-        <Form.Item label="5-ci Cavab - 12%">
-          <Input placeholder="Sorğu Cavabları" />
-        </Form.Item>
-      </Form>
+      {sorguData?.map((sorgu) => (
+        <div key={sorgu._id} id="dataList">
+          <div className="dataHeader">
+            <h4>
+              Sorğunun Başlığı: <span>{sorgu?.name}</span>
+            </h4>
+            <div className="dataDate">
+              <p>
+                <span>Tarix: </span> {sorgu?.createdAt?.slice(0, 10)}
+              </p>
+              <p>
+                <span>Son Tarix: </span> {sorgu?.expirationDate?.slice(0, 10)}
+              </p>
+            </div>
+          </div>
+          <div className="dataContent">
+            <div>
+              {sorgu?.options?.map((e, index) => {
+                return (
+                  <p key={index}>
+                    {e?.option} - <span>Cavab Verənlərin Sayı:</span>{" "}
+                    {e?.voteCount}
+                  </p>
+                );
+              })}
+            </div>
+            <div>
+              {sorgu?.result?.map((e, index) => {
+                return <p key={index}>- {e?.percentage} %</p>;
+              })}
+            </div>
+          </div>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 };
