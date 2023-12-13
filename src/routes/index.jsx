dd52f9cdc32a57cf6c7431947ebccd2ae234dcb3
Şəhriyar Məmmədov <76937208+Sehriyar4473@users.noreply.github.com>
2023-12-13 10:23:23 +0400

    routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/login";
 import AddNews from "../pages/addNews";
-import DashBoardPage from "../pages/dashBoard";
+import DashboardPage from "../pages/dashBoard";
 
 const Router = () => {
   return (
     <Routes>
       <Route index element={<Login />} />
-      <Route path="/admin/:id" element={<DashBoardPage />} />
+      <Route path="/admin/:id" element={<DashboardPage />} />
       <Route path="/admin/:id/addNews" element={<AddNews />} />
     </Routes>
   );
